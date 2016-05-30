5c6a06bea5c4462d7779f4c05768489fc0541138
gest01 <github@stefangeiger.ch>
2016-05-30 21:43:46 +0200

    Windows 8.1 App

---
+++ Demos/MobileApp/MobileApp.Windows81/MainPage.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+using Windows.Networking.PushNotifications;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// To add offline sync support, add the NuGet package Microsoft.WindowsAzure.MobileServices.SQLiteStore
+// to your project. Then, uncomment the lines marked // offline sync
+// For more information, see: http://aka.ms/addofflinesync
+//using Microsoft.WindowsAzure.MobileServices.SQLiteStore;  // offline sync
+//using Microsoft.WindowsAzure.MobileServices.Sync;         // offline sync
+
+namespace MobileApp
+{
+    sealed partial class MainPage: Page
+    {
+        private MobileServiceUser user;
+        private MobileServiceCollection<TodoItem, TodoItem> items;
+        private IMobileServiceTable<TodoItem> todoTable = App.MobileService.GetTable<TodoItem>();
+        //private IMobileServiceSyncTable<TodoItem> todoTable = App.MobileService.GetSyncTable<TodoItem>(); // offline sync
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async Task InsertTodoItem(TodoItem todoItem)
+        {
+            // This code inserts a new TodoItem into the database. When the operation completes
+            // and Mobile App backend has assigned an Id, the item is added to the CollectionView.
+            await todoTable.InsertAsync(todoItem);
+            items.Add(todoItem);
+
+            //await SyncAsync(); // offline sync
+        }
+
+        private async Task RefreshTodoItems()
+        {
+            MobileServiceInvalidOperationException exception = null;
+            try
+            {
+                // This code refreshes the entries in the list view by querying the TodoItems table.
+                // The query excludes completed TodoItems.
+                items = await todoTable
+                    .Where(todoItem => todoItem.Complete == false)
+                    .ToCollectionAsync();
+            }
+            catch (MobileServiceInvalidOperationException e)
+            {
+                exception = e;
+            }
+
+            if (exception != null)
+            {
+                await new MessageDialog(exception.Message, "Error loading items").ShowAsync();
+            }
+            else
+            {
+                ListItems.ItemsSource = items;
+                this.ButtonSave.IsEnabled = true;
+            }
+        }
+
+        private async void ButtonLogin_Click(object sender, RoutedEventArgs e)
+        {
+            // Login the user and then load data from the mobile app.
+            if (await AuthenticateAsync())
+            {
+                await InitNotificationsAsync();
+
+                // Switch the buttons and load items from the mobile app.
+                ButtonLogin.Visibility = Visibility.Collapsed;
+                ButtonSave.Visibility = Visibility.Visible;
+                //await InitLocalStoreAsync(); //offline sync support.
+                await RefreshTodoItems();
+            }
+        }
+
+        private async Task InitNotificationsAsync()
+        {
+            // Get a channel URI from WNS.
+            var channel = await PushNotificationChannelManager
+                .CreatePushNotificationChannelForApplicationAsync();
+
+            // Register the channel URI with Notification Hubs.
+            await App.MobileService.GetPush().RegisterAsync(channel.Uri);
+        }
+
+        private async Task UpdateCheckedTodoItem(TodoItem item)
+        {
+            // This code takes a freshly completed TodoItem and updates the database. When the service 
+            // responds, the item is removed from the list.
+            await todoTable.UpdateAsync(item);
+            items.Remove(item);
+            ListItems.Focus(Windows.UI.Xaml.FocusState.Unfocused);
+
+            //await SyncAsync(); // offline sync
+        }
+
+        private async void ButtonRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonRefresh.IsEnabled = false;
+
+            //await SyncAsync(); // offline sync
+            await RefreshTodoItems();
+
+            ButtonRefresh.IsEnabled = true;
+        }
+
+        private async void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            var todoItem = new TodoItem { Text = TextInput.Text };
+            await InsertTodoItem(todoItem);
+        }
+
+        private async void CheckBoxComplete_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = (CheckBox)sender;
+            TodoItem item = cb.DataContext as TodoItem;
+            await UpdateCheckedTodoItem(item);
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            //await InitLocalStoreAsync(); // offline sync
+            await RefreshTodoItems();
+        }
+
+        private async System.Threading.Tasks.Task<bool> AuthenticateAsync()
+        {
+            string message;
+            bool success = false;
+            try
+            {
+                // Change 'MobileService' to the name of your MobileServiceClient instance.
+                // Sign-in using Facebook authentication.
+                user = await App.MobileService
+                    .LoginAsync(MobileServiceAuthenticationProvider.MicrosoftAccount);
+                message =
+                    string.Format("You are now signed in - {0}", user.UserId);
+
+                success = true;
+            }
+            catch (InvalidOperationException)
+            {
+                message = "You must log in. Login Required";
+            }
+
+            var dialog = new MessageDialog(message);
+            dialog.Commands.Add(new UICommand("OK"));
+            await dialog.ShowAsync();
+            return success;
+        }
+
+        #region Offline sync
+
+        //private async Task InitLocalStoreAsync()
+        //{
+        //    if (!App.MobileService.SyncContext.IsInitialized)
+        //    {
+        //        var store = new MobileServiceSQLiteStore("localstore.db");
+        //        store.DefineTable<TodoItem>();
+        //        await App.MobileService.SyncContext.InitializeAsync(store);
+        //    }
+        //
+        //    await SyncAsync();
+        //}
+
+        //private async Task SyncAsync()
+        //{
+        //    await App.MobileService.SyncContext.PushAsync();
+        //    await todoTable.PullAsync("todoItems", todoTable.CreateQuery());
+        //}
+
+        #endregion 
+    }
+}
---
+++ Demos/MobileApp/MobileApp.Windows81/MainPage.xaml.cs
@@ -0,0 +1,8 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace MobileApp
+{
+    public sealed partial class MainPage : Page
+    {
+    }
+}
