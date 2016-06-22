dd0d1da67775ffe75c2e7bc5ee3c741ce5e28ce8
gest01 <github@stefangeiger.ch>
2016-06-22 21:24:28 +0200

    integration sql database

---
+++ Trivadis.AzureBootcamp.WebApi/Models/ChatDbContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Trivadis.AzureBootcamp.WebApi.Models
+{
+    public class ChatDbContext : DbContext
+    {
+        public ChatDbContext() : base("name=ChatDbContext")
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<ChatDbContext>());
+        }
+
+        public DbSet<ChatMessage> ChatMessages { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+
+    public class ChatMessage
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public String SenderUserId { get; set; }
+
+        [Required]
+        public String SenderUserName { get; set; }
+
+        public String Message { get; set; }
+        public String ImageUrl { get; set; }
+
+        [Required]
+        public DateTimeOffset Created { get; set; }
+
+        public static ChatMessage FromDto(ChatMessageDTO dto)
+        {
+            if (dto == null)
+                throw new ArgumentNullException(nameof(dto));
+
+            ChatMessage entity = new ChatMessage();
+            entity.ImageUrl = dto.ImageUrl;
+            entity.Message = dto.Message;
+            entity.SenderUserId = dto.SenderUserId;
+            entity.SenderUserName = dto.SenderUserName;
+            entity.Created = DateTimeOffset.Now;
+            return entity;
+        }
+    }
+}
